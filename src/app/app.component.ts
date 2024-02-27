d6fade8f9f81242106760b5e525575f39cb95258
Ricardo Varanda <rickyv08@gmail.com>
2024-02-27 09:50:20 +0000

    Upload changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export default class AppComponent implements OnInit {
   title = 'demo-ui';
+
+  constructor(private service: AppService) {}
+
+  ngOnInit(): void {
+    this.service
+      .getInfo()
+      .pipe(
+        map((res) => {
+          this.title = res.title;
+        })
+      )
+      .subscribe();
+  }
 }
